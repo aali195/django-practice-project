54ac1862c46dd0d8b8fd702e2d00e49dec7dccad
unknown <sgaali2@liverpool.ac.uk>
2019-02-28 03:47:18 +0000

    Fix class name in 'Listings' model

--- listings/models.py
+++ listings/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime
 from realtors.models import Realtor
 
-class listing(models.Model):
+class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
@@ -16,7 +16,7 @@ class listing(models.Model):
     garage = models.IntegerField(default=0)
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
-    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d')
     photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
