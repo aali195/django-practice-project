d12ee07538073c6d840edc10e91166d79ac29704
unknown <sgaali2@liverpool.ac.uk>
2019-03-04 00:19:40 +0000

    Add basic 'search' HTML page and linking'

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choices import price_choices, state_choices, bedroom_choices, price_choices
+
 from listings.models import Listing
 from realtors.models import Realtor
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     context = {
-        'listings': listings
+        'listings': listings,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
     }
     # Not in use for now, homepage will not show the recent listings
     return render(request, 'pages/home.html', context)
