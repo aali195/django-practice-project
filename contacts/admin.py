02448aaf0fbf46bbda6cd999d591f05ae93fe368
unknown <sgaali2@liverpool.ac.uk>
2019-03-05 03:13:03 +0000

    Add 'Contacts' model to 'admin' area

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contact
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    list_display_links = ('id', 'name')
+    list_per_page = 25
+
+admin.site.register(Contact, ContactAdmin)
